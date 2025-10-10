f85aca8b2737ac88e76f87e82956f1be974b36e4
subasri-sha <subashankar2004@gmail.com>
2025-10-10 22:59:08 +0530

    add hangman game implementation

---
+++ udemy_project/hangman.py
@@ -0,0 +1,112 @@
+import random
+
+hang_word = '''
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                       
+'''
+print(hang_word)
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+
+word_list = ["ardvark", "baboon", "camel"]
+lives = 6
+
+
+chosen_word = random.choice(word_list)
+# print(chosen_word)
+
+place_holder = ""
+word_length = len(chosen_word)
+
+for position in range(word_length):
+    place_holder += "_"
+print(place_holder)
+
+game_over = False
+correct_letters = [ ]
+
+while not game_over: # Use a while loop to allow the user to keep guessing until they find all the letters in the chosen_word.w
+    print("***************************************<???> / 6 LIVES LEFT ***************************************")
+    guess = input("Guess a letter: ").lower()
+
+
+    display = " "  # Create a display string which contains the letters and underscores
+   
+    for letter in chosen_word: 
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if guess not in chosen_word:
+        lives -=1
+        if lives == 0:
+            game_over = True
+            print("*************************************** YOU LOSE ***************************************")
+
+    if "_" not in display:
+        game_over = True
+        print("*************************************** YOU WON ***************************************")
+
+    print(HANGMANPICS[lives])
+
