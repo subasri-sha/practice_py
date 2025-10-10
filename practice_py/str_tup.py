0358d838c6349169cf75a576a67c864e29a41e2c
subasri-sha <subashankar2004@gmail.com>
2025-10-10 10:04:19 +0530

    add newline at end of file and format code for printing characters from a string

--- practice_py/str_tup.py
+++ practice_py/str_tup.py
@@ -5,3 +5,10 @@ result = () # empty tuple
 for char in name: 
     result += char, # adding , to make it a tuple
 print(result)
+
+# # Given name = "Python", print each character on a new line
+name = "Python"
+
+for char in name:
+    print(char)
+
